64c1761f1c221ac31bb4fad96bbe1b30b3752154
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-08-14 16:51:57 +0545

    fix

--- frontend/app/api/sign-cloudinary-params/route.ts
+++ frontend/app/api/sign-cloudinary-params/route.ts
@@ -7,14 +7,14 @@ import { v2 as cloudinary } from "cloudinary";
 // });
 
 cloudinary.config({
-  cloud_name: "duv0sr2ua",
-  api_key: "457615813374964",
-  api_secret: "36Ul4B5-9KAuBnsQBVAqr4MB2d0",
+  cloud_name: "db9vimrao",
+  api_key: "745115532881289",
+  api_secret: "rXZJIvoFw1FsS708gdAX8YrhCWI",
 });
 
-// export async function POST(request: Request) {
-//   const body = await request.json();
-//   const { paramsToSign } = body;
-//   const signature = cloudinary.utils.api_sign_request(paramsToSign, "rXZJIvoFw1FsS708gdAX8YrhCWI");
-//   return Response.json({ signature });
-// }
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { paramsToSign } = body;
+  const signature = cloudinary.utils.api_sign_request(paramsToSign, "rXZJIvoFw1FsS708gdAX8YrhCWI");
+  return Response.json({ signature });
+}
--- frontend/app/hooks/useCloudinaryFileUpload.ts
+++ frontend/app/hooks/useCloudinaryFileUpload.ts
@@ -1,67 +1,29 @@
-// "use client";
-// import { useState } from "react";
-// import { toast } from "sonner";
-
-// export default function useCloudinaryFileUpload() {
-//   const [uploading, setUploading] = useState(false);
-
-//   const handleFileUpload = (file: any, setImageUrl: any) => {
-//     if (file) {
-//       setUploading(true);
-//       const formData = new FormData();
-//       formData.append("file", file);
-//       formData.append("upload_preset", "jkh-preset");
-//       //  fetch("https://api.cloudinary.com/v1_1/dgnd6ay3m/image/upload", {
-//         fetch("https://api.cloudinary.com/v1_1/db9vimrao/image/upload", {
-
-//         method: "POST",
-//         body: formData,
-//       })
-//         .then((response) => response.json())
-//         .then((data) => {
-//           // Set the imageUrl using setImageUrl
-//           // console.log(data);
-//           // console.log(data.secure_url);
-//           // console.log(data.url);
-//           // setImageUrl(data.secure_url);
-//           setImageUrl(data.url);
-//         })
-//         .catch((error) => {
-//           console.error("Error uploading image:", error);
-//           toast.error("Error uploading image");
-//         })
-//         .finally(() => {
-//           setUploading(false);
-//         });
-//     }
-//   };
-
-//   return { uploading, handleFileUpload };
-// }
-
-
 "use client";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export default function useCloudinaryFileUpload() {
   const [uploading, setUploading] = useState(false);
-  const [imageUrl, setImageUrl] = useState<string>();
 
-  const handleFileUpload = (file: any) => {
+  const handleFileUpload = (file: any, setImageUrl: any) => {
     if (file) {
       setUploading(true);
       const formData = new FormData();
       formData.append("file", file);
       formData.append("upload_preset", "jkh-preset");
-      fetch("https://api.cloudinary.com/v1_1/db9vimrao/image/upload", {
+      //  fetch("https://api.cloudinary.com/v1_1/dgnd6ay3m/image/upload", {
+        fetch("https://api.cloudinary.com/v1_1/db9vimrao/image/upload", {
+
         method: "POST",
         body: formData,
       })
         .then((response) => response.json())
         .then((data) => {
           // Set the imageUrl using setImageUrl
-          console.log(data);
+          // console.log(data);
+          // console.log(data.secure_url);
+          // console.log(data.url);
+          // setImageUrl(data.secure_url);
           setImageUrl(data.url);
         })
         .catch((error) => {
@@ -74,5 +36,43 @@ export default function useCloudinaryFileUpload() {
     }
   };
 
-  return { uploading, handleFileUpload, imageUrl,setImageUrl };
+  return { uploading, handleFileUpload };
 }
+
+
+// "use client";
+// import { useState } from "react";
+// import { toast } from "sonner";
+
+// export default function useCloudinaryFileUpload() {
+//   const [uploading, setUploading] = useState(false);
+//   const [imageUrl, setImageUrl] = useState<string>();
+
+//   const handleFileUpload = (file: any, setImageUrl: unknown) => {
+//     if (file) {
+//       setUploading(true);
+//       const formData = new FormData();
+//       formData.append("file", file);
+//       formData.append("upload_preset", "jkh-preset");
+//       fetch("https://api.cloudinary.com/v1_1/duv0sr2ua/image/upload", {
+//         method: "POST",
+//         body: formData,
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           // Set the imageUrl using setImageUrl
+//           console.log(data);
+//           setImageUrl(data.url);
+//         })
+//         .catch((error) => {
+//           console.error("Error uploading image:", error);
+//           toast.error("Error uploading image");
+//         })
+//         .finally(() => {
+//           setUploading(false);
+//         });
+//     }
+//   };
+
+//   return { uploading, handleFileUpload, imageUrl,setImageUrl };
+// }
