f6550a4d0b57ed8abc019a66de2d260b585ea095
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-08-14 16:57:15 +0545

    fix

--- frontend/app/hooks/useCloudinaryFileUpload.ts
+++ frontend/app/hooks/useCloudinaryFileUpload.ts
@@ -12,8 +12,7 @@ export default function useCloudinaryFileUpload() {
       formData.append("file", file);
       formData.append("upload_preset", "jkh-preset");
       //  fetch("https://api.cloudinary.com/v1_1/dgnd6ay3m/image/upload", {
-        fetch("https://api.cloudinary.com/v1_1/db9vimrao/image/upload", {
-
+      fetch("https://api.cloudinary.com/v1_1/db9vimrao/image/upload", {
         method: "POST",
         body: formData,
       })
@@ -23,8 +22,8 @@ export default function useCloudinaryFileUpload() {
           // console.log(data);
           // console.log(data.secure_url);
           // console.log(data.url);
-          // setImageUrl(data.secure_url);
-          setImageUrl(data.url);
+          setImageUrl(data.secure_url);
+          // setImageUrl(data.url);
         })
         .catch((error) => {
           console.error("Error uploading image:", error);
@@ -38,41 +37,3 @@ export default function useCloudinaryFileUpload() {
 
   return { uploading, handleFileUpload };
 }
-
-
-// "use client";
-// import { useState } from "react";
-// import { toast } from "sonner";
-
-// export default function useCloudinaryFileUpload() {
-//   const [uploading, setUploading] = useState(false);
-//   const [imageUrl, setImageUrl] = useState<string>();
-
-//   const handleFileUpload = (file: any, setImageUrl: unknown) => {
-//     if (file) {
-//       setUploading(true);
-//       const formData = new FormData();
-//       formData.append("file", file);
-//       formData.append("upload_preset", "jkh-preset");
-//       fetch("https://api.cloudinary.com/v1_1/duv0sr2ua/image/upload", {
-//         method: "POST",
-//         body: formData,
-//       })
-//         .then((response) => response.json())
-//         .then((data) => {
-//           // Set the imageUrl using setImageUrl
-//           console.log(data);
-//           setImageUrl(data.url);
-//         })
-//         .catch((error) => {
-//           console.error("Error uploading image:", error);
-//           toast.error("Error uploading image");
-//         })
-//         .finally(() => {
-//           setUploading(false);
-//         });
-//     }
-//   };
-
-//   return { uploading, handleFileUpload, imageUrl,setImageUrl };
-// }
