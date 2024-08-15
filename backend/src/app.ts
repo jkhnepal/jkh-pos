bcece97a5c49788ba82c0b605b7a51b4c0715039
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-08-15 12:11:03 +0545

    ..

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,7 @@ app.use(express.json({ limit: "10kb" }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://jkh-pos.vercel.app/", "http://localhost:3002","https://jkh.webxnep.com", "https://pos-h8ki.vercel.app", "https://jackethouse.vercel.app","https://pos.jackethousenepal.com"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://jkh-pos.vercel.app/", "http://localhost:3002", "https://jkh.webxnep.com", "https://pos-h8ki.vercel.app", "https://jackethouse.vercel.app", "https://pos.jackethousenepal.com"],
     credentials: true,
   })
 );
